70a4e85249d93c49d3cdb819392a6367500c1ce4
Amanda <aamandaallinger@outlook.com>
2023-04-18 13:19:40 +0200

    Update content-effect.php

--- template-parts/content-effect.php
+++ template-parts/content-effect.php
@@ -1,31 +1,17 @@
 <main>
     <section>
-<div class="container">
-
-<div class="row">
-
-<div class="col-xs-12">
-
-<div class="hero">
-
-<img src="<?php the_post_thumbnail_url() ?>" alt="">
-
-<div class="text">
-
-<h1><?php the_title(); ?></h1>
-
-<p><?php the_content(); ?></p>
-
-</div>
-
-</div>
-
-</div>
-
-</div>
-
-</div>
-
-</section>
-
+        <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="hero">
+                    <img src="<?php the_post_thumbnail_url() ?>" alt="">
+                    <div class="text">
+                        <h1><?php the_title(); ?></h1>
+                        <p><?php the_content(); ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
 </main>

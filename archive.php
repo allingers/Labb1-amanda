472ba898ec33aa1c142a1f1d0bdb10e5af94c70b
Amanda <aamandaallinger@outlook.com>
2023-04-18 12:15:11 +0200

    Update archive.php

---
+++ archive.php
@@ -0,0 +1,61 @@
+<?php get_header(); ?>
+	<main>
+		<section>
+			<div class="container">
+				<div class="row">
+					
+					<div id="primary" class="col-xs-12 col-md-9">
+						<h1>
+						<?php 
+						//Funktion som ersätter behovet av flera sidor, som tex author.php, vid sökning
+						if (is_category()) {
+							echo "Blogg";
+						} elseif (is_tag()) {
+							single_tag_title();
+						} elseif (is_author()){
+							the_post();
+							echo get_the_author();
+							rewind_posts();
+						} else {
+							echo "Blogg";
+						}
+						?>
+						</h1>
+                        <?php 
+                        if( have_posts()){
+                            while(have_posts()){
+                                the_post();
+                                get_template_part('template-parts/content', 'archive');
+                            };
+                        };
+                        ?>
+							<nav class="navigation pagination">
+								<h2 class="screen-reader-text">Inläggsnavigering</h2>
+								<a class="prev page-numbers" href="">Föregående</a>
+								<span class="page-numbers current">1</span>
+								<a class="page-numbers" href="">2</a>
+								<a class="next page-numbers" href="">Nästa</a>
+							</nav>
+					</div>
+
+
+					<aside id="secondary" class="col-xs-12 col-md-3">
+						<div id="sidebar">
+								<ul role="navigation">
+									<li class="pagenav">
+										<?php if( is_active_sidebar('sidebar') ): ?>
+											<?php dynamic_sidebar('sidebar'); ?>
+										<?php endif;?>
+
+									</li>
+								</ul>
+																	
+							</ul>
+						</div>
+					</aside>
+				</div>
+			</div>
+		</section>
+	</main>
+
+<?php get_footer(); ?>

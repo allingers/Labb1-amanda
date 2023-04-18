9361162b19255a39e218200cb3323797bf1c58ba
Amanda <aamandaallinger@outlook.com>
2023-04-18 13:13:37 +0200

    Update 404.php

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+
+get_header();
+
+?>
+
+<main>
+    <section>
+        <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="text">
+                <h1>404 - Sidan du försöker nå finns inte... </h1>
+                <p>Klicka <a href="<?php echo site_url(); ?>"> HÄR </a> för att komma tillbaka till startsidan.</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+</main>
+
+<?php
+
+get_footer();
+
+?>

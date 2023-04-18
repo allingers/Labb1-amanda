e2ee664cf7b0779faaa3161a210ebd932376d796
Amanda <aamandaallinger@outlook.com>
2023-04-18 13:14:00 +0200

    Create content-effect.php
    mall för front-page

---
+++ template-parts/content-effect.php
@@ -0,0 +1,31 @@
+<main>
+    <section>
+<div class="container">
+
+<div class="row">
+
+<div class="col-xs-12">
+
+<div class="hero">
+
+<img src="<?php the_post_thumbnail_url() ?>" alt="">
+
+<div class="text">
+
+<h1><?php the_title(); ?></h1>
+
+<p><?php the_content(); ?></p>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+</main>
